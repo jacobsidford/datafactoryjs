eb494c430a6577662419dbeb70859a9dca38fcad
jacobsidford <jacob.sidford@gmail.com>
2020-06-25 16:29:18 +1200

    improved typing & added factory.createSingle()

--- src/factory.ts
+++ src/factory.ts
@@ -1,24 +1,32 @@
 export class DataFactory {
 	registeredTypes = new Map();
 
-	register(modelName: string, objectValues: Function) {
-		if (!this.registeredTypes.has(modelName)) {
-			this.registeredTypes.set(modelName, objectValues);
+	register<T>(modelName: string | (T & Function), objectValues: Function) {
+		const name = (typeof modelName === 'string'
+			? modelName
+			: (modelName as T & Function).name) as string;
+
+		if (!this.registeredTypes.has(name)) {
+			this.registeredTypes.set(name, objectValues);
 		} else {
-			throw new Error(`${modelName} already exists in data factory`);
+			throw new Error(`${name} already exists in data factory`);
 		}
 	}
 
-	create(
-		modelName: string,
+	create<T>(
+		modelName: string | (T & Function),
 		count: number = 1,
 		modelExtensions: object = {},
 		extendModel: boolean = false
-	): any[] {
-		const factoryModel = this.registeredTypes.get(modelName);
+	): T[] {
+		const name = (typeof modelName === 'string'
+			? modelName
+			: (modelName as T & Function).name) as string;
+
+		const factoryModel = this.registeredTypes.get(name);
 
 		if (!factoryModel) {
-			throw new Error(`${modelName} does not exist in data factory`);
+			throw new Error(`${name} does not exist in data factory`);
 		}
 
 		if (modelExtensions && !extendModel) {
@@ -28,14 +36,22 @@ export class DataFactory {
 			);
 		}
 
-		const objects = [];
+		const objects: T[] = [];
 		for (let i = count; i--; ) {
-			objects.push(this.instantiateObject(modelName, modelExtensions));
+			objects.push(this.instantiateObject(name, modelExtensions));
 		}
 		return objects;
 	}
 
-	instantiateObject(modelName: string, modelExtensions: object | null): object {
+	createSingle<T>(
+		modelName: string | (T & Function),
+		modelExtensions: object = {},
+		extendModel: boolean = false
+	): T {
+		return this.create<T>(modelName, 1, modelExtensions, extendModel)[0];
+	}
+
+	instantiateObject<T>(modelName: string, modelExtensions: object | null): T {
 		const model = this.registeredTypes.get(modelName);
 		return {
 			...model(),
